fcab254191eeb46d54ddcf3c11fdc07e133166ff
hglf <hgleifeng@foxmail.com>
2019-01-16 13:38:23 +0800

    before refacoter

--- app/src/main/java/github/hotstu/demo/hof/yoko/YokoTestActivity.java
+++ app/src/main/java/github/hotstu/demo/hof/yoko/YokoTestActivity.java
@@ -1,27 +1,89 @@
 package github.hotstu.demo.hof.yoko;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableInt;
+import androidx.databinding.ViewDataBinding;
 import github.hotstu.demo.hof.R;
+import github.hotstu.demo.hof.BR;
 import github.hotstu.lib.hof.yokohama.YokoView;
 
 @Route(path = "/app/yoko", name = "抽屉菜单")
 public class YokoTestActivity extends AppCompatActivity {
 
     private YokoView yokoView;
-
+    private ObservableInt currentSelect = new ObservableInt();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_yoko_test);
+        ViewDataBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_yoko_test);
+        binding.setVariable(BR.checkedId, currentSelect);
         yokoView = findViewById(R.id.yokoView);
+        yokoView.setApdater(new YokoView.YokoAdapter() {
+            @Override
+            public void onCollapsed(YokoView view) {
+                Log.d("hof", "onCollapsed");
+            }
+
+            @Override
+            public void onExpanded(YokoView view) {
+                Log.d("hof", "onExpanded");
+            }
+        });
+        yokoView.sync();
+    }
+
+
+    @BindingAdapter("bind:checked")
+    public static void setChecked(View v, int checkedId) {
+        if (v.getId() == checkedId) {
+            v.setSelected(true);
+        } else {
+            v.setSelected(false);
+        }
     }
 
     public void onClick(View view) {
-        yokoView.toggle();
+        if (currentSelect.get() == view.getId()) {
+            yokoView.toggle();
+            return;
+        }
+        if (view.getId() == R.id.toggle) {
+            yokoView.collapseThenExpand(() -> {
+                //here switch content of menu
+                TextView viewById = yokoView.getMenuView().findViewById(R.id.text);
+                viewById.setText("栏目0内容");
+            });
+        }
+        if (view.getId() == R.id.cte) {
+            yokoView.collapseThenExpand(() -> {
+                //here switch content of menu
+                TextView viewById = yokoView.getMenuView().findViewById(R.id.text);
+                viewById.setText("栏目1内容");
+            });
+        }
+        if (view.getId() == R.id.cte2) {
+            yokoView.collapseThenExpand(() -> {
+                //here switch content of menu
+                TextView viewById = yokoView.getMenuView().findViewById(R.id.text);
+                viewById.setText("栏目2内容");
+            });
+        }
+        if (view.getId() == R.id.cte3) {
+            yokoView.collapseThenExpand(() -> {
+                //here switch content of menu
+                TextView viewById = yokoView.getMenuView().findViewById(R.id.text);
+                viewById.setText("栏目3内容");
+            });
+        }
+        currentSelect.set(view.getId());
     }
 }
--- lib/src/main/java/github/hotstu/lib/hof/yokohama/YokoView.java
+++ lib/src/main/java/github/hotstu/lib/hof/yokohama/YokoView.java
@@ -17,41 +17,123 @@ import github.hotstu.lib.hof.R;
  */
 public class YokoView extends FrameLayout {
 
-    private View mView;
+    public interface YokoAdapter {
+        void onCollapsed(YokoView view);
+
+        void onExpanded(YokoView view);
+    }
+
+    private final FrameLayout mView;
+    private YokoAdapter mAdapter;
+    private boolean animating;
 
     public YokoView(@NonNull Context context) {
-        super(context);
-        init();
+        this(context, null);
     }
 
     public YokoView(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
+        this(context, attrs, 0);
     }
 
     public YokoView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        LayoutInflater.from(getContext()).inflate(R.layout.hof_yoko_container_layout, this, true);
+        mView = findViewById(R.id.container);
+        mView.setClickable(true);
+        setOnClickListener(v ->{
+            if (!animating) {
+                toggle();
+            }
+        });
     }
 
-    private void init() {
-        LayoutInflater.from(getContext()).inflate(R.layout.hof_yoko_test_layout, this, true);
-        mView = findViewById(R.id.test);
+
+    public void sync() {
+        setVisibility(isCollapsed() ? View.GONE : View.VISIBLE);
+    }
+
+    public void setApdater(YokoAdapter adapter) {
+        this.mAdapter = adapter;
+    }
+
+    private void performCallback(int type) {
+        if (mAdapter == null) {
+            return;
+        }
+        if (type == 0) {
+            mAdapter.onCollapsed(this);
+        }
+        if (type == 1) {
+            mAdapter.onExpanded(this);
+        }
+    }
+
+    public FrameLayout getMenuView() {
+        return mView;
+    }
+
+    public void collapse() {
+        mView.animate()
+                .translationY(-mView.getHeight())
+                .withStartAction(() -> {
+                    animating = true;
+                })
+                .withEndAction(() -> {
+                    animating = false;
+                    performCallback(0);
+                    setVisibility(View.GONE);
+                })
+                .start();
+    }
+
+    public boolean isCollapsed() {
+        return mView.getTranslationY() <= -mView.getHeight();
+    }
+
+    public boolean isExpanded() {
+        return mView.getTranslationY() >= 0;
+    }
+
+    public void expand(@Nullable Runnable beforeExpand) {
+        mView.animate()
+                .translationY(0)
+                .withStartAction(() -> {
+                    animating = true;
+                    setVisibility(View.VISIBLE);
+                    if (beforeExpand != null) {
+                        beforeExpand.run();
+                    }
+                })
+                .withEndAction(() -> {
+                    animating = false;
+                    performCallback(1);
+                })
+                .start();
+    }
+
+
+    public void collapseThenExpand(@Nullable Runnable beforeExpand) {
+        if (isCollapsed()) {
+            expand(beforeExpand);
+        } else {
+            mView.animate()
+                    .translationY(-mView.getHeight())
+                    .withStartAction(() -> {
+                        animating = true;
+                    })
+                    .withEndAction(() -> {
+                        animating = false;
+                        expand(beforeExpand);
+                    })
+                    .start();
+        }
     }
 
     public void toggle() {
-        float translationY = mView.getTranslationY();
-        if (translationY != 0) {
-            mView.animate()
-                    .translationY(0)
-                    .start();
-        } else  {
-            mView.animate()
-                    .translationY(-mView.getHeight())
-                    .start();
-
+        if (isCollapsed()) {
+            expand(null);
+        } else {
+            collapse();
         }
-        //mView.getMeasuredHeight();
-
     }
 }
